cb4c21e31f9770c42ac4bb5b52cb0bf6e30cb0f2
前原聖吾 <maeharaseigo@maeharakiyoshiwarenoMacBook-Air.local>
2024-04-30 17:13:02 +0900

    changed return print > print

--- chap2/2-5.py
+++ chap2/2-5.py
@@ -4,23 +4,23 @@ class Line:
         self.arr2 = arr2
     
     def next(self, s):
-        return print(str(self.arr1[self.arr1.index(s) + 1]))
+        print(str(self.arr1[self.arr1.index(s) + 1]))
     
     def prev(self, s):
-        return print(str(self.arr1[self.arr1.index(s) - 1]))
+        print(str(self.arr1[self.arr1.index(s) - 1]))
     
     def next_exp(self, s):
-        return print(str(self.arr2[self.arr2.index(s) + 1]))
+        print(str(self.arr2[self.arr2.index(s) + 1]))
     
     def prev_exp(self, s):
-        return print(str(self.arr2[self.arr2.index(s) - 1]))
+        print(str(self.arr2[self.arr2.index(s) - 1]))
 
 
 class Line2(Line):
     def next(self, s):
         if s not in self.arr1:
             return "no such station exists."
-        elif s == self.arr1(-1):
+        elif s == self.arr1[-1]:
             return "input is a terminal station."
         else:
             super().next(s)
@@ -28,7 +28,7 @@ class Line2(Line):
     def prev(self, s):
         if s not in self.arr1:
             return "no such station exists."
-        elif s == self.arr1(0):
+        elif s == self.arr1[0]:
             return "input is a terminal station."
         else:
             super().prev(s)
@@ -36,24 +36,22 @@ class Line2(Line):
     def next_exp(self, s):
         if s not in self.arr1:
             return "no such station exists."
+        elif s not in self.arr2:
+            return "express does not stop at this station."
+        elif s == self.arr2[-1]:
+            return "input is a terminal station."
         else:
-            if s not in self.arr2:
-                return "express does not stop at this station."
-            elif s == self.arr2(-1):
-                return "input is a terminal station."
-            else:
-                super(). next_exp(s)
+            super().next_exp(s)
 
     def prev_exp(self, s):
         if s not in self.arr1:
             return "no such station exists."
+        elif s not in self.arr2:
+            return "express does not stop at this station."
+        elif s == self.arr2[0]:
+            return "input is a terminal station."
         else:
-            if s not in self.arr2:
-                return "express does not stop at this station."
-            elif s == self.arr2(0):
-                return "input is a terminal station."
-            else:
-                super().prev_exp(s)
+            super().prev_exp(s)
 
 
 exec(input())
